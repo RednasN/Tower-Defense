388fd1376c5491928aa9ca1a842c5b2cc38da28b
RednasN <sander1992nijhof@gmail.com>
2026-03-13 13:18:41 +0100

    Add ProjectileType enum to define various projectile types

---
+++ src/app/models/projectiles/projectile-type.model.ts
@@ -0,0 +1,9 @@
+export enum ProjectileType {
+  Base = 'Base',
+  Rocket = 'Rocket',
+  Bullet = 'Bullet',
+  Laser = 'Laser',
+  NuclearBullet = 'NuclearBullet',
+  SlowRocket = 'SlowRocket',
+  GrenadeMine = 'GrenadeMine',
+}
